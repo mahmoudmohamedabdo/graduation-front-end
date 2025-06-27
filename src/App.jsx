cb2dc0dfc5a2f927feda9a6057d441cc0284bc6c
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-06-27 16:52:27 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <div className=" bg-blue-100 flex items-center justify-center p-5">
+      <h1 className="text-4xl font-bold text-purple-700">Tailwind is working! 🚀</h1>
+    </div>
+  );
+}
+
+export default App
